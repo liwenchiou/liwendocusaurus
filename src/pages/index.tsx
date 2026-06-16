e155cac2db482aad2b6b33aee71e4f286659f084
liwen <aa771117@gmail.com>
2026-06-16 14:50:48 +0800

    fix: update homepage hero button link to docs/intro

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,7 +23,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/next-js-notes/day-01">
+            to="/docs/intro">
             技術筆記 🛠️
           </Link>
           <Link
